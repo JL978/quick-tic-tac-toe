00719e58750a7d38c1fbcf2fcb800a4d60d28caf
Jimmy Lam <JimmyLam978@gmail.com>
2020-05-15 10:09:17 -0400

    Could add more functionality like reset and all that but we'll call it done for now

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import sys
-from PyQt5 import QtWidgets
+import time
+from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtWidgets import QDialog, QApplication, QGridLayout, QPushButton, QWidget, QSizePolicy
 from PyQt5.QtCore import pyqtSignal, QTimer, QThread
 
+x_icon = 'x-icon.png'
+o_icon = 'o-icon.png'
 
 class board(QWidget):
     ROWS = 3
@@ -11,7 +14,8 @@ class board(QWidget):
         super(board, self).__init__()
         self.setWindowTitle('Tic Tac Toe')
         self.setFixedSize(300,300)
-        self.positions = [[0 for i in range(self.ROWS)] for _ in range(self.COLLUMNS)]
+        self.setWindowIcon(QtGui.QIcon('logo.png'))
+        self.positions = [[0 for _ in range(self.ROWS)] for _ in range(self.COLLUMNS)]
         self.create_grid()
         self.turns = {'X': 'O', 'O':'X'}
         self.player = 'X'
@@ -21,13 +25,14 @@ class board(QWidget):
         self.setLayout(self.gridLayout)
         self.gridLayout.setSpacing(0)
         
-
+        self.button_grid = [[None for _ in range(self.ROWS)] for _ in range(self.COLLUMNS)]
         for row, l in enumerate(self.positions):
-            for collumn, _ in enumerate(l):
-                button = infoButton(row, collumn)
+            for column, _ in enumerate(l):
+                button = infoButton(row, column)
                 button.position.connect(self.on_click)
                 button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-                self.gridLayout.addWidget(button, row, collumn)
+                self.button_grid[row][column] = button
+                self.gridLayout.addWidget(button, row, column)
     
     def on_click(self, position):
         row, column = position 
@@ -35,9 +40,13 @@ class board(QWidget):
             print('Illegal move')
         else:
             self.positions[row][column] = self.player
+            if self.player == 'X':
+                self.button_grid[row][column].setIcon(QtGui.QIcon(x_icon))
+            else:
+                self.button_grid[row][column].setIcon(QtGui.QIcon(o_icon))
             self.check_winner(self.positions, self.player)
             self.player = self.turns[self.player]
-            print('\n'.join([str(x) for x in self.positions]))
+            #print('\n'.join([str(x) for x in self.positions]))
 
     def check_winner(self, board, player):
         if board[0][0] == board[0][1] == board[0][2] == player or \
@@ -49,10 +58,12 @@ class board(QWidget):
             board[0][0] == board[1][1] == board[2][2] == player or \
             board[0][2] == board[1][1] == board[2][0] == player:
             print(f'{player} win')
-            self.destroy()
+            self.close()
+
         elif 0 not in board[0] and 0 not in board[1] and 0 not in board[2]:
             print('No one wins') 
-            self.destroy()
+            self.close()
+    
                 
 
 
